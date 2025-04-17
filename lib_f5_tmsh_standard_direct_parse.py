c87b35eda38e34ba5e82393d82dc38dea2d66e6b
samualblair <10884846+samualblair@users.noreply.github.com>
2025-04-17 11:22:44 -0700

    continued wip on dictionary conversino for bigip.conf

--- lib_f5_tmsh_standard_direct_parse.py
+++ lib_f5_tmsh_standard_direct_parse.py
@@ -3,57 +3,94 @@
 # Initial idea from nsmcan @DevCentral
 # See [https:]//community[.]f5[.]com/discussions/technicalforum/any-python-experts-out-there---parsing-tmsh-list/297634
 
-declarative_keys = ['disabled', 'internal', 'ip-forward', 'vlans-enabled']
+import json
 
-def parse_tmsh_one_line_output(out):
+def parse_layer(tokens:list):
+    """
+    Parses one dictionary layer from the tokens
+    @param tokens: string tokens
+    @return: parsed dictionary, leftover tokens
+    """
+    
+    DECLARATIVE_KEYS = ['disabled', 'internal', 'ip-forward', 'vlans-enabled', 'count', 
+                        'socks4', 'socks4a', 'socks5', 'avg_tps', 'avg_request_throughput',
+                        '__', 'errors', 'drop_count', 'total_attacks_count', 'events_count', 
+                        'acl_matches','total_count','psm_protocol_type','FTP', 'SMTP']
+    TMSH_TOP_LEVEL_KEYS = ['apm', 'asm', 'ltm']
+    TMSH_TWO_COMMANDS = ['apm aaa']
+    TMSH_THREE_COMMANDS = ['apm aaa active-directory']
+
+    result = {}
+    is_key = True  # The first token will be a key
+    key = None
+    while tokens:
+        token = tokens.pop(0)
+
+        # if token in TMSH_TOP_LEVEL_KEYS:
+        #     print("MATCHED TOP LEVEL")
+        #     if (token + ' ' + tokens[0]) in TMSH_TWO_COMMANDS:
+        #         print("MATCHED TWO COMMANDS")
+        #     if (token + ' ' + tokens[0] + ' ' + tokens[1]) in TMSH_THREE_COMMANDS:
+        #         print("MATCHED THREE COMMANDS")
+
+        if token in TMSH_TOP_LEVEL_KEYS:
+            if (token + ' ' + tokens[0]) in TMSH_TWO_COMMANDS:
+                if (token + ' ' + tokens[0] + ' ' + tokens[1]) in TMSH_THREE_COMMANDS:
+                    #print("MATCHED THREE COMMANDS")
+                    #print(tokens)
+                    token = token + ' ' + tokens[0] + ' ' + tokens[1] + ' ' + tokens[2]
+                    tokens.pop(0)
+                    tokens.pop(0)
+                    tokens.pop(0)
+                    #print(tokens)
+
+        if is_key:
+            key = token
+            if key in DECLARATIVE_KEYS:
+                result[key] = True
+                continue
+            if key == '}':
+                #remaining_tokens = len(tokens)
+                #if remaining_tokens > 0:
+                #    print(tokens)
+                break
+            is_key = False
+        else:
+            value = token
+            is_key = True
+            if value == '{':
+                result[key], tokens = parse_layer(tokens)
+                continue
+            if value.startswith('"'):
+                tokens.insert(0, value[1:])
+                parts = []
+                while tokens:
+                    token = tokens.pop(0)
+                    if token.endswith('"') and not token.endswith('\\"'):
+                        parts.append(token[:-1])
+                        break
+                    parts.append(token)
+                value = ' '.join(parts)
+            # Catch value "}"
+            # if value == '}':
+            #     print('This KEY needs to have a value: '+key)
+            result[key] = value.replace('\\"', '"')
+
+    # end while
+    return result, tokens
+
+def parse_tmsh_one_line_output(singleline_parse:str):
     """
     Parses output of a tmsh command produced in machine-readable format with the 'one-line' option
-    @param out: Command output
+    @param singleline_parse: String of Command output
     @return: Parsed dictionary
     """
-    def parse_layer(tokens):
-        """
-        Parses one dictionary layer from the tokens
-        @param list tokens: string tokens
-        @return: parsed dictionary, leftover tokens
-        """
-        result = {}
-        is_key = True  # The first token will be a key
-        key = None
-        while tokens:
-            token = tokens.pop(0)
 
-            if is_key:
-                key = token
-                if key in declarative_keys:
-                    result[key] = True
-                    continue
-                if key == '}':
-                    break
-                is_key = False
-            else:
-                value = token
-                is_key = True
-                if value == '{':
-                    result[key], tokens = parse_layer(tokens)
-                    continue
-                if value.startswith('"'):
-                    tokens.insert(0, value[1:])
-                    parts = []
-                    while tokens:
-                        token = tokens.pop(0)
-                        if token.endswith('"') and not token.endswith('\\"'):
-                            parts.append(token[:-1])
-                            break
-                        parts.append(token)
-                    value = ' '.join(parts)
-                result[key] = value.replace('\\"', '"')
-
-        # end while
-        return result, tokens
-
-    _tokens = out.split(' ')
+    _tokens = singleline_parse.split(' ')
     parsed, _ = parse_layer(_tokens)
+    #if _:
+        #print("Warning There were leftover tokens:")
+        #print(_)
     return parsed
 
 def convert_tmsh_output_to_oneline(config_input):
@@ -116,3 +153,24 @@ def convert_tmsh_output_to_oneline(config_input):
                     current_line = current_line + text
         
     return list_of_tmsh_lines
+
+def print_conversion_normal(bigipconf_filename:str="bigip.conf"):
+    
+    list_normal = []
+
+    with open(bigipconf_filename, 'r') as bigipconf_configfile:
+        
+        
+        working_file = bigipconf_configfile.read()
+
+        f5_normal_config_converted = convert_tmsh_output_to_oneline(working_file)
+
+        list_normal = []
+
+        for lines in f5_normal_config_converted:
+            test_f5_normal_config_split = parse_tmsh_one_line_output(lines)
+            list_normal.append(test_f5_normal_config_split)
+
+    with open(bigipconf_filename[:-5]+'_dict.json', 'w') as bigipconf_dict_file:
+        json_string = json.dumps(list_normal, indent=4)
+        bigipconf_dict_file.write(json_string)
