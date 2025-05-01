bfcb1601400f16c19f929bb83003760723d2e3d9
samualblair <10884846+samualblair@users.noreply.github.com>
2025-05-01 13:01:15 -0700

    Added ucs migration workflow 1

---
+++ f5_ucs_migration_workflows/1_ucs_ve_maxcoreupdate/1_extract_modify_rearchive.py
@@ -0,0 +1,162 @@
+# Created by: Michael Johnson - 04-21-2025
+# Parsing code to extract big-ip ucs, update bigdbdat file, and re-archive
+import os
+import subprocess
+import configparser
+from stat import S_IRUSR, S_IRGRP, S_IROTH, S_IWUSR
+
+def extract_bigip_archive(bigip_conf_filename:str='support.qkview',file_extention_length:int=7) -> str:
+    """
+    Extracts All Files from bigip archive - qkview, ucs, generic tar.gz
+    Will return string with folder path of extracted (with original name + _unpacked)
+    """
+    path_of_new_folder = (f'{bigip_conf_filename[0:len(bigip_conf_filename)-file_extention_length]}_unpacked')
+
+    try:
+        os.mkdir(path_of_new_folder)
+    except FileExistsError:
+        print('Folder already existed')
+
+    sub_comamnd = f'tar -xzf "{bigip_conf_filename}" -C "{path_of_new_folder}"'
+    subprocess.run(sub_comamnd, shell=True)
+    
+    return path_of_new_folder
+
+def update_maxcores(bigdb_filename:str='BigDB.dat',file_extention_length:int=4) -> None:
+    """
+    Directly updates BigDB.dat file
+    """
+
+    # Using config parser to read in values and work with them
+    bigdb_config = configparser.ConfigParser()
+
+    # Read in config file (BigDB.dat)
+    bigdb_config.read(bigdb_filename)
+
+    # If not present, will raise 'KeyError'
+    try:
+        # Inform user file already have value set
+        print('[License.MaxCores] value= is already set to', bigdb_config['License.MaxCores']['value'],'in the file',bigdb_filename)
+        # Nothing left to do so return
+        return
+
+    except KeyError:
+        # Key needs to be set - inform users it will be set
+        print('Updating [License.MaxCores] with value=8 in the file',bigdb_filename)
+
+        bigdb_config['License.MaxCores']['value'] = '8'
+
+        # This makes the file read/write for the owner, Read for Group, Read for Other
+        os.chmod(bigdb_filename, S_IWUSR|S_IRUSR|S_IRGRP|S_IROTH)
+
+        # Will Fail if file is marked as read-only
+        with open(bigdb_filename, 'w') as bigdb_configfile:
+            bigdb_config.write(bigdb_configfile, space_around_delimiters=False)
+
+        temp_file_contents = ""
+        # Remove blank lines to allow easier error checking by admin
+        with open(bigdb_filename, 'r') as bigdb_configfile:
+            temp_file_contents = bigdb_configfile.read().replace('\n\n', '\n')
+        with open(bigdb_filename, 'w') as bigdb_configfile:
+            bigdb_configfile.write(temp_file_contents)
+
+        # This makes the file read only again (User, Group,)
+        os.chmod(bigdb_filename, S_IRUSR|S_IRGRP|S_IROTH)
+
+def archive_ucs(bigip_conf_folder:str='support.qkview',file_extention_length:int=9) -> None:
+    """
+    Archives previously extracted files and folders from bigip archive (ucs, generic tar.gz) into a new ucs archive based on input folder path
+    """
+
+    # New file name should be old path without '_unpacked' or -9 charachters, then add '_new.ucs' to end
+    path_of_new_ucs_file = (f'{bigip_conf_folder[0:len(bigip_conf_folder)-file_extention_length]}_new.ucs')
+
+    # Inform User of Creation Start
+    print(f'Starting Creating of archive: "{path_of_new_ucs_file}" from "{bigip_conf_folder}"')
+
+    # Create Archive
+    sub_comamnd = f'tar -czf "{path_of_new_ucs_file}" "{bigip_conf_folder}/"'
+    subprocess.run(sub_comamnd, shell=True)
+
+    # Archive Created
+    print(f'Finished archive: {path_of_new_ucs_file}')
+
+
+if __name__ == "__main__":
+    # Assign starting directory to recursivly work in
+    directory = input('Please enter folder name to parse all files within (HINT: may navigate back a folder with ../FOLDERNAME )\n')
+
+    # Iterate over files in the directory, Walk directory tree and record for later use
+    recursive_folder_list = []    
+    for currentpath, folders, files in os.walk(directory):
+        for folder in folders:
+            # print(os.path.join(currentpath, file))
+            recursive_folder_list.append(os.path.join(currentpath, folder))
+    # Also add base folder to list
+    recursive_folder_list.append(directory)
+
+    # Extract UCS files found
+    # Store list of extracted folders for future reference
+    extracted_folders_list = []
+    try:
+        for folder_name in recursive_folder_list:
+            for file_name in os.listdir(folder_name):
+                file_contents = os.path.join(folder_name, file_name)
+                # checking if it is a file
+                if os.path.isfile(file_contents):
+                    try:
+                        # Only try to parse file if it is name ends with .ucs or .tar.gz
+                        if file_name[-7:] == ".tar.gz":
+                            # Extract archive and store returned extracted folder
+                            extracted_folders_list.append(extract_bigip_archive(file_contents))
+                        elif file_name[-4:] == ".ucs":
+                            # Extract archive and store returned extracted folder
+                            extracted_folders_list.append(extract_bigip_archive(file_contents,4))
+                    # Catch when file is not parsable UTF 8 or similar
+                    except UnicodeDecodeError:
+                        print('Fail to read file - ' + file_name + ' : Is this a file to be read?')
+    except IndexError:
+        print('Issue with file - ' + file_name)
+
+    # # Modify Bigip.dat files - should be known location inside exctracted folder - so direct edit is possible
+    # # Concerns about folder pathing on different OSes (LINUX/MACOS/UNIX/POSIX use '/' and WINDOWS uses '\') , only defined for non-windows
+    # for extracted_folder in extracted_folders_list:
+    #     bigip_dat_filename = extracted_folder + '/config/BigDB.dat'
+    #     try:
+    #         update_maxcores(bigip_dat_filename)                            
+    #     # Catch when file is not parsable UTF 8 or similar
+    #     except UnicodeDecodeError:
+    #         print('Fail to read file - ' + bigip_dat_filename + ' : Is this a file to be read?')
+    #     except IndexError:
+    #         print('Issue with file - ' + bigip_dat_filename)
+
+    # Modify Bigip.dat files
+    # Using discovery also helps potentially make script more os portable in future to other OSes
+    # Iterate over files in the previous directories, Walk directory tree and record for later use
+    extracted_recursive_folder_list = []
+    for extracted_folder in extracted_folders_list:
+        for currentpath, folders, files in os.walk(extracted_folder):
+            for folder in folders:
+                # print(os.path.join(currentpath, file))
+                extracted_recursive_folder_list.append(os.path.join(currentpath, folder))
+        # Also add base folder to list
+        extracted_recursive_folder_list.append(extracted_folder)
+    try:
+        for folder_name in extracted_recursive_folder_list:
+            for file_name in os.listdir(folder_name):             
+                file_contents = os.path.join(folder_name, file_name)
+                # checking if it is a file
+                if os.path.isfile(file_contents):
+                    try:
+                        # Only try to parse file if it is name ends with "BigDB.dat"
+                        if file_name[-9:] == "BigDB.dat":
+                            update_maxcores(file_contents)                            
+                    # Catch when file is not parsable UTF 8 or similar
+                    except UnicodeDecodeError:
+                        print('Fail to read file - ' + file_name + ' : Is this a file to be read?')
+    except IndexError:
+        print('Issue with file - ' + file_name)
+
+    # Re-Archive in new UCS
+    for folder_name in extracted_folders_list:
+        archive_ucs(folder_name)
---
+++ f5_ucs_migration_workflows/1_ucs_ve_maxcoreupdate/USAGE.md
@@ -0,0 +1,83 @@
+# F5 UCS Migration Workflows - 1 - Specific Updates only Required (aka Physical to VE)
+
+This workflow supports taking multiple source UCS files, extracting, modifying, and reachiving for use on new system.
+Specificially this is for minimal changes that need to be made, that can all be predicted.
+Currently that means only changes needed to alllow a physical UCS to load into a VE (changes to Bigdb.dat file).
+
+## Modify UCS to migrate physical to VE - Required Process:
+
+Export .ucs files from legacy F5s (multiple sets of config might exist).
+  - Upload it to a shared location, or directly to the workstation that will be performing workflow.
+  - Example: Place .ucs files under <file-share-root>\legacy_ucs_configs\<site>\original
+  - Preferably Name the file with hostname, date, version
+    
+    EXAMPLE:
+    ```
+    old_hostnameA-01_04012025_v15.ucs
+    old_hostnameB-02_04012025_v15.ucs
+    ```
+
+Download .ucs file to local folder (best to not work with extracting/re-archiving on remote location)
+  - Example: Place .ucs files under <local-folder-root>\legacy_ucs_configs\<site>\original
+
+Execute Script 1 against original .ucs file
+  - Navigate to script file location (or call out script via full path)
+  - Run script 1 (python3 1_extract_modify_rearchive.py)
+  - Provide script path for .ucs files
+  - Script will complete and will provide extracted copies of all ucs contents for each source device
+  - Script will also go through and will modify 'License.MaxCores' value to 8 for each Bigip.dat files
+  - Script will also re-archive each file
+  - At this point:
+    - Each device will have its own folder (<ucsname>_unpacked) , this can be safely removed
+    - Each device will have its own _updated ucs file , this will be kept
+
+Cleanup: Remove the 'unpacked' folder
+  - Not needed - can be safely delted
+
+Optional (for less confusion) Move new .ucs to 'updated' folder.
+  - Example: Place .ucs files under <local-folder-root>\legacy_ucs_configs\<site>\updated
+
+Optional (for working in a group) Upload new .ucs files to remote folder
+  - Example: Place .ucs files under <file-share-root>\legacy_ucs_configs\<site>\original
+
+Load on new device (or lab for testing)
+  - Ensure new device has a required license level
+  - Ensure new device has correct master key
+  - Copy / import the new .ucs file to BigIP and laod.
+
+
+## Script Overviews
+
+## Usage Example for Script 1:
+```bash
+# run script , and call current folder
+python3.13 "1_extract_modify_rearchive.py"
+Please enter folder name to parse all files within (HINT: may navigate back a folder with ../FOLDERNAME )
+./
+# No Status is shown as extractions are happening
+# Script will print on screen status as it updates files or skips over them (for any BigDB.dat file it finds)
+Updating [License.MaxCores] with value=8 in the file ./ucs_test/f5_1_backup_unpacked/config/BigDB.dat
+Updating [License.MaxCores] with value=8 in the file ./ucs_test/f5_1_backup_unpacked/config/.diffVersions/config/BigDB.dat/BigDB.dat
+Updating [License.MaxCores] with value=8 in the file ./ucs_test/f5_2_backup_unpacked/config/BigDB.dat
+Updating [License.MaxCores] with value=8 in the file ./ucs_test/f5_2_backup_unpacked/config/.diffVersions/config/BigDB.dat/BigDB.dat
+# Script will print on scresn status as it rearchive
+Starting Creating of archive: "./ucs_test/f5_1_backup_new.ucs" from "./ucs_test/f5_1_backup_unpacked"
+Finished archive: ./ucs_test/f5_1_backup_new.ucs
+Starting Creating of archive: "./ucs_test/f5_2_backup_new.ucs" from "./ucs_test/f5_2_backup_unpacked"
+Finished archive: ./ucs_test/f5_2_backup_new.ucs
+❯
+
+# Does not currently remove the 'extracted folder' for you, so feel free to cleanup if not needed
+
+# Newly packed UCS files can be seen , as well as 'extracted' folders (_unpacked)
+❯ ls -1 -l ucs_test
+total 122312
+-rw-r--r--@ 1 michaelj  staff  15119032 Apr 17 12:22 f5_1_backup_new.ucs
+drwxr-xr-x@ 9 michaelj  staff       288 Apr 17 12:22 f5_1_backup_unpacked
+-rwx------@ 1 michaelj  staff  15939632 Aug  7  2024 f5_1_backup.ucs
+-rw-r--r--@ 1 michaelj  staff  15081087 Apr 17 12:22 f5_2_backup_new.ucs
+drwxr-xr-x@ 9 michaelj  staff       288 Apr 17 12:22 f5_2_backup_unpacked
+-rw-r--r--@ 1 michaelj  staff  15076962 Aug  9  2024 f5_2_backup.ucs
+
+❯
+```
