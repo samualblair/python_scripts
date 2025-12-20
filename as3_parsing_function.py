4901dcb75dbe053830a7cf3fbb10ce1361856605
samualblair <10884846+samualblair@users.noreply.github.com>
2025-12-20 12:55:21 -0800

    error cleanup and service address class handled

--- as3_parsing_function.py
+++ as3_parsing_function.py
@@ -1,6 +1,7 @@
-# Michael W. Johnson - 7-26-2024
+# Michael W. Johnson - 12-20-2025
 # Paring code to parse existing AS3 JSON file and filter or modify information
-# This code Specifically provides a function to parse through AS3 JSON, find missing Service Port (member port) information, and add a service port based on Virtual Port (VS Port).
+# This code Specifically provides a function to parse through AS3 JSON and perform tasks and saving in well formatted JSON when done
+# One task is to find missing Service Port (member port) information, and add a service port based on Virtual Port (VS Port).
 
 import json
 import csv
@@ -11,12 +12,12 @@ virtual_port_dict = {}
 # in_filename = 'example_good_as3.json'
 # in_filename = 'example_bad_as3.json'
 # output_filename = 'new_fixed_output.json'
-# output_filename = 'new_nochange_output.json'
+# output_filename = 'new_no_change_output.json'
 
 def Add_pool_service_port(in_filename) -> None:
     '''
     Parses F5 AS3 JSON File, finds missing member service port information, adds service port based on Virtual Server Port in use, and saves back to the file.
-    This is distructive of original file, please backup file prior to running.
+    This is destructive of original file, please backup file prior to running.
     '''
 
     with open(in_filename, 'r') as vars_file:
@@ -33,7 +34,7 @@ def Add_pool_service_port(in_filename) -> None:
 
                             # Try to find the Current Service Port
                             try:
-                                name_of_application = source['declaration'][dec_key][tenant_key][application_key]
+                                # name_of_application = source['declaration'][dec_key][tenant_key][application_key]
                                 class_of_application = source['declaration'][dec_key][tenant_key][application_key]['class']
                                 if class_of_application == "Service_HTTP":
                                     apps_found.append(class_of_application)
@@ -41,7 +42,7 @@ def Add_pool_service_port(in_filename) -> None:
                                 # print(name_of_application_port)
                                 virtual_port_dict['virtualPort_found'] = name_of_application_port
                                 # print(apps_found)
-                            except:
+                            except KeyError:
                                 pass
 
                             # print(source['declaration'][dec_key][tenant_key][application_key])
@@ -52,24 +53,34 @@ def Add_pool_service_port(in_filename) -> None:
                                     if source['declaration'][dec_key][tenant_key][application_key][app_object_key] == 'Pool':
                                         try:
                                             for member in source['declaration'][dec_key][tenant_key][application_key]['members']:
-                                                null_value = member['servicePort']
-                                                # pass
-                                        except:
+                                                # null_value = member['servicePort']
+                                                pass
+                                        except KeyError:
+                                            pass
                                             # print("Member has no Service Port - " + str(member))
-                                            current_app = source['declaration'][dec_key][tenant_key]
+                                            # current_app = source['declaration'][dec_key][tenant_key]
                                             # print(current_app)
                                             
                                             print("Adding")
                                             for member in source['declaration'][dec_key][tenant_key][application_key]['members']:
                                                 member['servicePort'] = virtual_port_dict['virtualPort_found']
                                                 # print(member['servicePort'])                                       
-                                except:
-                                    pass
 
-                    except:
+                                except TypeError:
+                                    pass
+                                except KeyError:
+                                    pass
+                    except TypeError:
                         pass
-            except:
+                    except KeyError:
+                        pass
+            except TypeError:
                 pass
+            except KeyError:
+                pass
+            except Exception as error:
+                print(type(error).__name__)
+                print(error)
 
 
 
@@ -82,7 +93,7 @@ def Add_pool_service_port(in_filename) -> None:
 def Update_vip_ip(in_filename) -> None:
     '''
     Parses F5 AS3 JSON File, and CSV IP old/new file, to swap out old NS VIP IPs with new F5 VIP IPs.
-    This is distructive of original file, please backup file prior to running.
+    This is destructive of original file, please backup file prior to running.
     '''
 
     csv_dic = {}
@@ -107,29 +118,70 @@ def Update_vip_ip(in_filename) -> None:
 
                             # Try to find the Current Service Port
                             try:
-                                name_of_application = source['declaration'][dec_key][tenant_key][application_key]
+                                # name_of_application = source['declaration'][dec_key][tenant_key][application_key]
                                 class_of_application = source['declaration'][dec_key][tenant_key][application_key]['class']
-                                if class_of_application == "Service_HTTP":
-                                    apps_found.append(class_of_application)
-                                name_of_application_ip = source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses']
-                                string_old_ip = 'OLD IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'])
-                                # print("Found - Attempting to Swap")
+                                
+                                # if class_of_application == "Service_HTTP":
+                                #     apps_found.append(class_of_application)
 
-                                # Set the lookup key (first element in list of IPs, assuming only 1 IP aka 1 element)
-                                lookup_key = name_of_application_ip[0]
+                                if class_of_application[0:8] == 'Service_':
+                                    # Catch most Service classes with Service Address
+                                    if class_of_application != 'Service_Address':
+                                        apps_found.append(class_of_application)
+                                        try:
+                                            name_of_application_ip = source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses']
+                                            string_old_ip = 'OLD IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'])
+                                            #print("Found - Attempting to Swap")
+                                            #print(name_of_application_ip + ' and ' + string_old_ip)
 
-                                # Update the address to the new value, from lookup of old/new dictionary
-                                source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'] = [ csv_dic[lookup_key] ]
-                                string_new_ip = 'NEW IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'])
-                                print(string_old_ip + ' -> ' + string_new_ip)
+                                            # Set the lookup key (first element in list of IPs, assuming only 1 IP aka 1 element)
+                                            lookup_key = name_of_application_ip[0]
+                                            #print(lookup_key)
 
-                            except:
+                                            # Update the address to the new value, from lookup of old/new dictionary
+                                            source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'] = [ csv_dic[lookup_key] ]
+                                            string_new_ip = 'NEW IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddresses'])
+                                            print(string_old_ip + ' -> ' + string_new_ip)
+                                        except KeyError:
+                                            # print("Exception on IP VS Service Addresses Swap: " + class_of_application)
+                                            pass
+                                    # Catch case of dedicated Service Address class
+                                    else:
+                                        apps_found.append(class_of_application)
+                                        try:
+                                            name_of_application_ip = source['declaration'][dec_key][tenant_key][application_key]['virtualAddress']
+                                            string_old_ip = 'OLD IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddress'])
+                                            #print("Found - Attempting to Swap")
+                                            #print(name_of_application_ip + ' and ' + string_old_ip)
+
+                                            # Set the lookup key (first element in list of IPs, assuming only 1 IP aka 1 element)
+                                            lookup_key = name_of_application_ip
+                                            #print(lookup_key)
+
+                                            # Update the address to the new value, from lookup of old/new dictionary
+                                            source['declaration'][dec_key][tenant_key][application_key]['virtualAddress'] = csv_dic[lookup_key]
+                                            # Print message informing update was made
+                                            string_new_ip = 'NEW IP:' + str(source['declaration'][dec_key][tenant_key][application_key]['virtualAddress'])
+                                            print(string_old_ip + ' -> ' + string_new_ip)
+                                        except KeyError:
+                                            # print("Exception on IP Service Address: " + class_of_application)
+                                            pass
+
+                            except TypeError:
                                 pass
-
-                    except:
+                            except KeyError:
+                                pass
+                    except TypeError:
                         pass
-            except:
+                    except KeyError:
+                        pass
+            except TypeError:
                 pass
+            except KeyError:
+                pass
+            except Exception as error:
+                print(type(error).__name__)
+                print(error)
 
     # output_filename = str(in_filename + '_updated.json')
     output_filename = str(in_filename)
@@ -140,7 +192,7 @@ def Update_vip_ip(in_filename) -> None:
 def Update_tenant_name(in_filename) -> None:
     '''
     Parses F5 AS3 JSON File, and CSV IP old/new file, to swap out old Tenant (partition) name.
-    This is potentially distructive of original file, please backup file prior to running.
+    This is potentially destructive of original file, please backup file prior to running.
     '''
 
     destination = {}
@@ -164,7 +216,7 @@ def Update_tenant_name(in_filename) -> None:
             try:
                 for tenant_key in source['declaration'][dec_key]:
                     try:
-                        apps_found = []
+                        # apps_found = []
                         
                         for application_key in source['declaration'][dec_key][tenant_key]:
 
@@ -176,33 +228,40 @@ def Update_tenant_name(in_filename) -> None:
                                 #name_of_app_container = source['declaration'][dec_key][tenant_key]
 
                                 # Important to keep these as they ensure only running loop inside an application
-                                name_of_application = source['declaration'][dec_key][tenant_key][application_key]
-                                class_of_application = source['declaration'][dec_key][tenant_key][application_key]['class']
+                                # name_of_application = source['declaration'][dec_key][tenant_key][application_key]
+                                # class_of_application = source['declaration'][dec_key][tenant_key][application_key]['class']
 
                                 # Check if app name ends with '-app' or not, only run if it does
                                 if '-app' == tenant_key[-4:]:
                                     #print('a match')
 
-                                    # Contsruct new name, by replacing (lb_) with (t_), simply by dropping first letter 3 charachters of name, and ignore ending 4 (-app)
+                                    # Construct new name, by replacing (lb_) with (t_), simply by dropping first letter 3 characters of name, and ignore ending 4 (-app)
                                     # Current logic loop will do this for all apps, so only last app name remains
                                     new_tenant_name = 't_' + tenant_key[3:-4]
                                     destination['declaration'][new_tenant_name] = source['declaration'][dec_key]
                                     destination['declaration'].pop(dec_key)
 
-                                    # Contsruct new name, by replacing (lb_) with (a_), simply by dropping first letter 3 charachters of name, and ignore ending 4 (-app)
+                                    # Construct new name, by replacing (lb_) with (a_), simply by dropping first letter 3 characters of name, and ignore ending 4 (-app)
                                     # Current logic loop will do this for all apps, so only last app name remains
                                     new_app_name = 'a_' + tenant_key[3:-4]
                                     destination['declaration'][new_tenant_name][new_app_name] = source['declaration'][dec_key][tenant_key]
                                     destination['declaration'][new_tenant_name].pop(tenant_key)
 
-                            except:
+                            except TypeError:
                                 pass
-
-                    except:
+                            except KeyError:
+                                pass
+                    except TypeError:
                         pass
-
-            except:
+                    except KeyError:
+                        pass
+            except TypeError:
                 pass
+            except KeyError:
+                pass
+            except Exception as error:
+                print(type(error).__name__)
+                print(error)
 
     # output_filename = str(in_filename + '_updated.json')
     output_filename = str(in_filename)
@@ -213,13 +272,13 @@ def Update_tenant_name(in_filename) -> None:
 def Update_schema(in_filename) -> None:
     '''
     Parses F5 AS3 JSON File, and CSV IP old/new file, to swap out old schema and schemaVersion.
-    This is distructive of original file, please backup file prior to running.
+    This is destructive of original file, please backup file prior to running.
     '''
 
     with open(in_filename, 'r') as vars_file:
         source = json.load(vars_file)
 
-        source['declaration']['schemaVersion'] = "3.46.2"
+        source['declaration']['schemaVersion'] = "3.54.0"
         source['$schema'] = "https://raw.githubusercontent.com/F5Networks/f5-appsvcs-extension/main/schema/latest/as3-schema.json"
 
     # output_filename = str(in_filename + '_updated.json')
@@ -233,7 +292,7 @@ def Count_vips(in_filename) -> None:
     Parses F5 AS3 JSON File, Counts IP on VS, Prints output on console
     '''
 
-    conunt_dic = 0
+    count_dic = 0
 
     with open(in_filename, 'r') as vars_file:
         source = json.load(vars_file)
@@ -246,28 +305,36 @@ def Count_vips(in_filename) -> None:
                         for application_key in source['declaration'][dec_key][tenant_key]:
                             # Try to find the Current Service Port
                             try:
-                                name_of_application = source['declaration'][dec_key][tenant_key][application_key]
+                                # name_of_application = source['declaration'][dec_key][tenant_key][application_key]
                                 class_of_application = source['declaration'][dec_key][tenant_key][application_key]['class']
                                 if class_of_application[0:8] == 'Service_':
-                                    conunt_dic += 1
-                                    #print(class_of_application)
-                                    #print(name_of_application)
-
-                            except:
+                                    if class_of_application != 'Service_Address':
+                                        count_dic += 1
+                                        #print(class_of_application)
+                                        #print(name_of_application)
+                            except TypeError:
                                 pass
-
-                    except:
+                            except KeyError:
+                                pass
+                    except TypeError:
                         pass
-            except:
+                    except KeyError:
+                        pass
+            except TypeError:
                 pass
-
+            except KeyError:
+                pass
+            except Exception as error:
+                print(type(error).__name__)
+                print(error)
     if __name__ == "__main__":
-        print(f'The total number of VS are: {conunt_dic}')
+        print(f'The total number of VS are: {count_dic}')
     else:
-        return(conunt_dic)
+        return(count_dic)
 
 
 if __name__ == "__main__":
+    # Testing with single file run as script rather than imported function
     Add_pool_service_port('example_good_as3.json')
     Update_vip_ip('example_good_as3.json')
     Update_tenant_name('example_good_as3.json')
